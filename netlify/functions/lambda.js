edc8053d0ea8a1d89329fb26eb48dc05be9e7031
GiacomoBorsellino <giacomoborsellino@hotmail.it>
2021-07-16 11:51:21 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,15 +1,23 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
+
   const key =  process.env.key;
 
   const fet = await fetch(`https://api.themoviedb.org/3/movie/${event.queryStringParameters.value}?api_key=${key}&language=en-US`)
   const response = await fet.json() 
 
   const pass = (response) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response)
+    if (fet.ok) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      }      
+    } else {
+        return {
+          statusCode: 404,
+          body: JSON.stringify(response)
+        }        
     }
   }
 
